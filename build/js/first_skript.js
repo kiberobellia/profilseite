3652b03873e416ccbc97243977b0d5a405e1df6f
kiberobellia <philipp.bellia@kibero.com>
2024-04-06 10:59:54 +0200

    Ältere Änderungen jetzt übernommen vom Laptop

---
+++ build/js/first_skript.js
@@ -0,0 +1,10 @@
+// erstes skript
+
+
+const name = "Philipp";
+const nachname = "Bellia";
+let alter = 24;
+
+document.write("Hallo, mein Name ist " + name + " " + nachname + " und ich bin " + alter + " Jahre alt." );
+
+let eingabe = prompt();
